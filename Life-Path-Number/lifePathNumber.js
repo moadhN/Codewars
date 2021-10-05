fb9fb3b8e2d8bf8570d52c4d41efd6a4436c07bc
moadhN <m.programer1919@gmail.com>
2021-10-05 16:39:18 +0100

    add lifePathNumber.js

---
+++ Life-Path-Number/lifePathNumber.js
@@ -0,0 +1,44 @@
+function lifePathNumber(dateOfBirth) {
+    let num = dateOfBirth.split('-').join('');
+    do {
+        num = ('' + num).split('').reduce((a, b) => +a + +b);
+    } while (num > 9)
+
+    return num;
+}
+
+/* --------------------------------------------------------------------------------------------------------- */
+
+function lifePathNumber(dateOfBirth) {
+    console.log(dateOfBirth)
+    return dateOfBirth.replace(/\D/g, '') % 9 || 9;
+}
+
+/* --------------------------------------------------------------------------------------------------------- */
+
+function lifePathNumber(dateOfBirth) {
+    let y = [], m = [], d = [], lifenumY = 0, lifenumM = 0, lifenumD = 0, lifenum = 0;
+    dateOfBirth = dateOfBirth.split('-')
+    y = dateOfBirth[0]
+    m = dateOfBirth[1]
+    d = dateOfBirth[2]
+    do {
+        lifenumY = sum(y)
+        y = lifenumY + ''
+    } while (lifenumY > 9)
+    do {
+        lifenumM = sum(m)
+        m = lifenumM + ''
+    } while (lifenumM > 9)
+    do {
+        lifenumD = sum(d)
+        d = lifenumD + ''
+    } while (lifenumD > 9)
+    lifenum = lifenumY + lifenumM + lifenumD
+    while (lifenum > 9) {
+        string = lifenum + ''
+        lifenum = sum(string)
+
+    }
+    return lifenum
+}
