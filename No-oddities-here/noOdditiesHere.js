7dd00e48906708dbf8ce0bbe5c6d9b81beb8acb1
moadhN <m.programer1919@gmail.com>
2021-10-07 22:38:34 +0100

    edit noOdditiesHere.js

--- No-oddities-here/noOdditiesHere.js
+++ No-oddities-here/noOdditiesHere.js
@@ -1,1 +1,13 @@
 const noOdds = values => values.filter(val => !(val % 2));
+/* --------------------------------------------------------------------------------------------------------- */
+
+function noOdds(values) {
+    let myArray = [];
+    for (let i = 0; i < values.length; i++) {
+        let n = values[i]
+        if (n % 2 == 0) {
+            myArray.push(n)
+        }
+    }
+    return myArray
+}
