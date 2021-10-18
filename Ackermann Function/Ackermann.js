771ba14ba3f24735f97246e1d012c4799ec3f1fb
moadhN <m.programer1919@gmail.com>
2021-10-18 20:45:09 +0100

    Ackermann.js

---
+++ Ackermann Function/Ackermann.js
@@ -0,0 +1,8 @@
+const Ackermann = function (m, n) {
+    if (m === 0) return n + 1
+    else if (m > 0 && n === 0) return Ackermann(m - 1, 1)
+    else if (m > 0 && n > 0) return Ackermann(m - 1, Ackermann(m, n - 1))
+    else {
+        return null
+    }
+}
