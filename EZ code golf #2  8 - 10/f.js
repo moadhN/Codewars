c0c451f87ffa03463580d7ff4759f040ad75dbf0
moadhN <m.programer1919@gmail.com>
2021-11-06 04:55:09 +0100

    nthEven.js

---
+++ EZ code golf #2  8 - 10/f.js
@@ -0,0 +1,3 @@
+function f(n) {
+    return parseInt(Number(n), 2)
+}
--- Get Nth Even Number/nthEven.js
+++ Get Nth Even Number/nthEven.js
@@ -1,11 +1,3 @@
 function nthEven(n) {
-    let i = 0;
-    let count = 0;
-    while (count < n) {
-        if (i % 2 == 0) {
-            count++
-        }
-        i++
-    }
-    return count
+    return (n - 1) * 2
 }
